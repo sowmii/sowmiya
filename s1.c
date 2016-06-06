5a11f624fb9c2684718c97b230a49223fd796212
sowmii <sowmiimail@gmail.com>
2016-06-06 23:13:31 +0530

    Update and rename .gitignore to s1.c

---
+++ s1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	printf("enter number\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	printf("\n number is even");
+	else
+	 printf("\nnumber is odd");
+}
