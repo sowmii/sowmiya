ab897ebe3fd194b9d55390cba252378508ae1e19
sowmii <sowmiimail@gmail.com>
2016-06-06 23:09:22 +0530

    Update and rename .gitignore to s.c

---
+++ s.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	printf("enter number\n");
+	scanf("%d",&n);
+	if(n==0)
+	printf("\n number is zero");
+	if(n>0)
+	 printf("\nnumber is positive");
+	 if(n<0)
+	 printf("\n number is negative");
+	return 0;
+}
